a81f039b3aecff1610588d72adfe896dafb7bbeb
Peter Mbanugo <peter@foobar.agency>
2022-04-30 23:52:41 +0200

    initial setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const fastify = require("fastify")({
+  logger: true,
+});
+const todos = require("./todo.json");
+
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+fastify.listen(3000, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`);
+});
